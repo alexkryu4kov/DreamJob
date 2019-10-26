d840128c23cc83c61541877c4abebc7a41bfa35d
alexkryu4kov <alexkryu4kov@gmail.com>
2019-10-27 03:56:10 +0700

    Добавлена логика в получение запроса про скиллы

--- src/app/models/skills.py
+++ src/app/models/skills.py
@@ -1,5 +1,21 @@
 class SkillsPredictor:
-    def post_skills(self, data):
+    def post_skills(self, data, db):
+        print(data)
+        known = []
+        unknown = []
+        email = data['email']
+        skills = data['skills']
+        for skill in skills:
+            if skill['selected']:
+                known.append(skill['name'])
+            else:
+                unknown.append(skill['name'])
+        self.save_data(db, email, known, unknown)
         return {
             'status': 'OK',
         }
+
+    def save_data(self, db, email, known, unknown):
+        db.cur.execute(f"INSERT INTO email_known (email, known) VALUES ('{email}', '{elem}')" for elem in known)
+        db.cur.execute(f"INSERT INTO email_unknown (email, unknown) VALUES ('{email}', '{elem}')" for elem in unknown)
+        db.conn.commit()
