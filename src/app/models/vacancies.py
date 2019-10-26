8acc26edd618eb5bd34775f64073e6acdc709692
alexkryu4kov <alexkryu4kov@gmail.com>
2019-10-27 03:23:18 +0700

    Из вакансии получаются реальные скиллы

--- src/app/models/vacancies.py
+++ src/app/models/vacancies.py
@@ -1,5 +1,5 @@
 class VacanciesPredictor:
-    def create_list_of_vacancies(self, data):
+    def create_list_of_vacancies(self, db, data):
         return [
             {
                 'name': row.name,
@@ -7,11 +7,16 @@ class VacanciesPredictor:
                 'company_name': row.company_name,
                 'city': row.city,
                 'salary': row.salary,
-                'skills': ['Python', 'Web', 'Java']
+                'skills': self.get_list_of_skills(db, row.url)
             } for row in data
         ]
 
+    def get_list_of_skills(self, db, url):
+        db.cur.execute(f"SELECT * FROM skills WHERE vacancy_url='{url}'")
+        data = db.cur.fetchall()
+        return [row.skill for row in data]
+
     def get_vacancies(self, name, level, db):
-        db.cur.execute(f"SELECT * FROM vacancies WHERE name='{name}' AND level='{level.capitalize()}';")
+        db.cur.execute(f"SELECT * FROM vacancies WHERE name='{name}' AND level='{level}';")
         row_data = db.cur.fetchall()
-        return self.create_list_of_vacancies(row_data)
+        return self.create_list_of_vacancies(db, row_data)
