714740d98bc0bdc3bd1396b097fa379aff81f0a6
alexkryu4kov <alexkryu4kov@gmail.com>
2019-10-26 15:40:49 +0700

    Возвращается список вакансий

--- src/app/models/vacancies.py
+++ src/app/models/vacancies.py
@@ -1,10 +1,30 @@
 class VacanciesPredictor:
     def get_vacancies(self, name, level):
         return {
-            'name': f'{name} developer',
-            'level': level,
-            'company_name': 'CFT',
-            'city': 'Moscow',
-            'salary': 31000,
-            'skills': ['Python', 'Web', 'Java'],
+            'vacancies_list': [
+                {
+                    'name': f'{name} developer',
+                    'level': level,
+                    'company_name': 'CFT',
+                    'city': 'Moscow',
+                    'salary': 31000,
+                    'skills': ['Python', 'Web', 'Java']
+                },
+                {
+                    'name': f'{name} developer',
+                    'level': level,
+                    'company_name': 'CFT',
+                    'city': 'Moscow',
+                    'salary': 60000,
+                    'skills': ['Python', 'Web', 'Java']
+                },
+                {
+                    'name': f'{name} developer',
+                    'level': level,
+                    'company_name': 'CFT',
+                    'city': 'Moscow',
+                    'salary': 80000,
+                    'skills': ['Python', 'Web', 'Java']
+                },
+            ]
         }
