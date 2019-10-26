6ad6b9e886571aa235d6b3d98c99767cc4b08e3a
alexkryu4kov <alexkryu4kov@gmail.com>
2019-10-26 20:03:50 +0700

    Конфиг вынесен в отдельный файл

---
+++ src/app/external/config.py
@@ -0,0 +1,8 @@
+db_config = {
+    'user': 'jcoqxhnbbdmhej',
+    'password': '6634869b448b0e129b789480e55230d35087d0bca4760416e7ac3aae61654982',
+    'host': 'ec2-174-129-231-116.compute-1.amazonaws.com',
+    'port': 5432,
+    'dbname': 'da7ht06l1b358e',
+    'sslmode': 'require'
+}
