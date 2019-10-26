566b93b9a96e49df828d7d98693aab860caa342e
alexkryu4kov <alexkryu4kov@gmail.com>
2019-10-26 11:14:25 +0700

    Правки под gunicorn

--- src/app/service.py
+++ src/app/service.py
@@ -1,19 +1,19 @@
 from aiohttp.web import Application
 from aiohttp import web
 
-from app import settings
+from app.settings import setup_profile_predictor, setup_skills_predictor, setup_spec_predictor, setup_vacancies_predictor
 
 from app.routes import setup_routes
 
 
-def init_app():
+async def init_app():
     """Иницализация приложения."""
     app = Application()
     setup_routes(app)
-    app.on_startup.append(settings.setup_spec_predictor)
-    app.on_startup.append(settings.setup_vacancies_predictor)
-    app.on_startup.append(settings.setup_skills_predictor)
-    app.on_startup.append(settings.setup_profile_predictor)
+    app.on_startup.append(setup_spec_predictor)
+    app.on_startup.append(setup_vacancies_predictor)
+    app.on_startup.append(setup_skills_predictor)
+    app.on_startup.append(setup_profile_predictor)
     return app
 
 
