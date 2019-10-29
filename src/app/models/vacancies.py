0b59be59c34569fbbdfe4e7d701d991a31dc1c8d
Alexey Kryuchkov <al.krjuchkov@cft.ru>
2019-10-29 13:07:58 +0700

    Отладка работы с бд

--- src/app/models/vacancies.py
+++ src/app/models/vacancies.py
@@ -17,6 +17,10 @@ class VacanciesPredictor:
         return list(set([row.skill for row in data]))
 
     def get_vacancies(self, name, level, db):
+        db.cur.execute(f"SELECT * FROM vacancies;")
+        test_data = db.cur.fetchall()
+        print(test_data)
         db.cur.execute(f"SELECT * FROM vacancies WHERE name='{name}' AND level='{level}';")
         row_data = db.cur.fetchall()
+        print(row_data)
         return self.create_list_of_vacancies(db, row_data)
