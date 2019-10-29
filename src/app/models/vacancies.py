5d827ddfa56d34482b9f67072d34e7ab2390171a
Alexey Kryuchkov <al.krjuchkov@cft.ru>
2019-10-29 16:59:06 +0700

    Отладка работы с бд

--- src/app/models/vacancies.py
+++ src/app/models/vacancies.py
@@ -17,6 +17,14 @@ class VacanciesPredictor:
         return list(set([row.skill for row in data]))
 
     def get_vacancies(self, name, level, db):
+        email = 'Leha@idi.na'
+        db.cur.execute(f"SELECT * FROM email_known WHERE email={email}")
+        data = db.cur.fetchall()
+        print('data', data)
+        email = 'Leha@idi.na'
+        db.cur.execute(f"SELECT * FROM email_known WHERE email='{email}'")
+        test_data = db.cur.fetchall()
+        print('test_data', test_data)
         db.cur.execute(f"SELECT * FROM vacancies WHERE name='{name}' AND level='{level}';")
         row_data = db.cur.fetchall()
         return self.create_list_of_vacancies(db, row_data)
