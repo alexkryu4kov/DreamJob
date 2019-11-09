952c0026213fd02f89168075d270116848d525dc
alexkryu4kov <alexkryu4kov@gmail.com>
2019-11-10 00:28:50 +0700

    Создается connection pool вместо отдельного соединения

--- src/app/external/config.py
+++ src/app/external/config.py
@@ -4,5 +4,5 @@ db_config = {
     'host': 'ec2-174-129-231-116.compute-1.amazonaws.com',
     'port': 5432,
     'database': 'da7ht06l1b358e',
-    'ssl': 'require'
+    'ssl': 'require',
 }
--- src/app/external/db.py
+++ src/app/external/db.py
@@ -2,4 +2,4 @@ import asyncpg
 
 
 async def init_database(pg_config):
-    return await asyncpg.connect(**pg_config)
+    return await asyncpg.create_pool(**pg_config)
