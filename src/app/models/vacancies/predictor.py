d7629acc6d8aa02a58b866dcd6f23736a6a3a814
alexkryu4kov <alexkryu4kov@gmail.com>
2019-11-09 15:26:11 +0700

    Рефакторинг модуля vacancies

--- src/app/models/vacancies/predictor.py
+++ src/app/models/vacancies/predictor.py
@@ -1,20 +1,47 @@
-class VacanciesPredictor:
-    async def create_list_of_vacancies(self, db, data):
-        return [
-            {
-                'name': row['name'],
-                'level': row['level'],
-                'company_name': row['company_name'],
-                'city': row['city'],
-                'salary': row['salary'],
-                'skills': await self.get_list_of_skills(db, row['url'])
-            } for row in data
-        ]
+# TODO: написать докстринги
+# TODO: использовать проперти
+# TODO: инкапсулировать взаимодействие с бд
+# TODO: работать с классами, а не со словарями
+# TODO: сделать интеллектуальный парсер скиллов из вакансий
 
-    async def get_list_of_skills(self, db, url):
-        data = await db.fetch(f"SELECT * FROM skills WHERE vacancy_url='{url}'")
-        return list(set([row['skill'] for row in data]))
+
+class VacanciesPredictor:
+
+    def __init__(self):
+        self._name = None
+        self._level = None
+        self._db = None
+
+    def _set_db(self, db):
+        self._db = db
+
+    def _set_name(self, name):
+        self._name = name
+
+    def _set_level(self, level):
+        self._level = level
 
     async def get_vacancies(self, name, level, db):
-        row_data = await db.fetch(f"SELECT * FROM vacancies WHERE name='{name}' AND level='{level}';")
-        return await self.create_list_of_vacancies(db, row_data)
+        self._set_db(db)
+        self._set_name(name)
+        self._set_level(level)
+        row_data = await self._db.fetch(f"SELECT * FROM vacancies WHERE name='{self._name}' AND level='{self._level}';")
+        return await self.create_list_of_vacancies(row_data)
+
+    async def _create_dict_from_row(self, row):
+        return {
+            'name': row['name'],
+            'level': row['level'],
+            'company_name': row['company_name'],
+            'city': row['city'],
+            'salary': row['salary'],
+            'skills': await self._get_list_of_skills(row['url'])
+        }
+
+    async def create_list_of_vacancies(self, data):
+        return [self._create_dict_from_row(row) for row in data]
+
+    async def _get_list_of_skills(self, url):
+        data = await self._db.fetch(f"SELECT * FROM skills WHERE vacancy_url='{url}'")
+        return list(set([row['skill'] for row in data]))
+
