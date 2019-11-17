c027bff8742cef0fef148055662b3f0e6b337b57
alexkryu4kov <alexkryu4kov@gmail.com>
2019-11-18 00:34:51 +0700

    Небольшой рефакторинг

--- src/app/models/profile/score/profile_score.py
+++ src/app/models/profile/score/profile_score.py
@@ -1,4 +1,5 @@
 from app.models.profile.known.known_from_db import KnownFromDb
+from app.models.profile.score.count_score import count_score
 from app.models.profile.unknown.unknown_from_db import UnknownFromDb
 
 
@@ -12,12 +13,5 @@ class ProfileScore:
         known_data = await self._known_from_db.select_known(email)
         unknown_data = await self._unknown_from_db.select_unknown(email)
         return {
-            'score': self.count_score(known_data, unknown_data),
+            'score': count_score(known_data, unknown_data),
         }
-
-    def count_score(self, known: list, unknown: list) -> float:
-        try:
-            score = len(known) / (len(known) + len(unknown))
-            return round(score, 2)
-        except ZeroDivisionError:
-            return 0.0
