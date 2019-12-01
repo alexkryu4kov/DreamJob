81c611a79cfb281ffcb25dd4e23dde32b1bc3b48
alexkryu4kov <alexkryu4kov@gmail.com>
2019-12-01 15:37:10 +0700

    Исправлен SELECT * FROM

--- src/app/models/profile/courses/courses_from_db.py
+++ src/app/models/profile/courses/courses_from_db.py
@@ -8,7 +8,7 @@ class CoursesFromDb(Db):
     async def select_courses(self, skill):
         await self._make_connection()
         courses = await self._connection.fetch(
-            '''SELECT * FROM courses WHERE skill=$1''',
+            '''SELECT url, name FROM courses WHERE skill=$1''',
             skill,
         )
         await self._close_connection()
--- src/app/models/profile/known/known_from_db.py
+++ src/app/models/profile/known/known_from_db.py
@@ -8,7 +8,7 @@ class KnownFromDb(Db):
     async def select_users(self, email):
         await self._make_connection()
         users = await self._connection.fetch(
-            '''SELECT * FROM users WHERE email=$1 ORDER BY save_time desc''',
+            '''SELECT save_time FROM users WHERE email=$1 ORDER BY save_time desc''',
             email,
         )
         await self._close_connection()
@@ -17,7 +17,7 @@ class KnownFromDb(Db):
     async def select_known(self, email: str) -> list:
         await self._make_connection()
         known = await self._connection.fetch(
-            '''SELECT * FROM email_known WHERE email=$1 ORDER BY save_time desc''',
+            '''SELECT known, save_time FROM email_known WHERE email=$1 ORDER BY save_time desc''',
             email,
         )
         await self._close_connection()
